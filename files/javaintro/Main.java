4b1751a7c0a4e23a1c7c3ef985b8526d60fde480
Filipp Voronov <53707586+pjanoh@users.noreply.github.com>
2021-12-10 13:03:11 +0300

    Update Main.java

--- files/javaintro/Main.java
+++ files/javaintro/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         int income = 70000;   // доход
-        int outcome = 100000; // расход
-        int tax = (income - outcome) * 15 / 100; // налог 15% на разницу доходов и расходов
+        int spending = 100000; // расход
+        int tax = (income - spending) * 15 / 100; // налог 15% на разницу доходов и расходов
 
         System.out.println("Ваш налог: " + tax + " рублей");
 
